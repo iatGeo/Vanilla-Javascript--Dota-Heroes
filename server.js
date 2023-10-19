660171a468397038be350a6f51fd3067f39574a5
iatGeo <g.iatridis@protonmail.com>
2023-10-19 17:38:14 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+
+let heroes = {
+    'abaddon': {
+        'type': 'Universal',
+        'mainInfo': `SHIELDS HIS ALLIES OR HIMSELF FROM ATTACKS`,
+        'attackType': 'Melee',
+        'complexity': 1,
+        'spells': ['Mist Coil', 'Aphotic Shield', 'Curse of Avernus', 'Borrowed time']
+    },
+    'alchemist': {
+        'type': 'Strength',
+        'mainInfo': `EARNS EXTRA GOLD FROM UNIT KILLS AND BOUNTY RUNES`,
+        'attackType': 'Melee',
+        'complexity': 1,
+        'spells': ['Acid Spray', 'Unstable Concoction', 'Corrosive Weaponry', 'Chemical Rage']
+    },
+    'ancient apparition': {
+        'type': 'Intelligence',
+        'mainInfo': `LAUNCHES A POWERFUL ICY BLAST FROM ANYWHERE ON THE MAP`,
+        'attackType': 'Ranged',
+        'complexity': 2,
+        'spells': ['Cold Feet', 'Ice Vortex', 'Chilling Touch', 'Ice Blast']
+    },
+    'antimage': {
+        'type': 'Agility',
+        'mainInfo': `SLASHES HIS FOES WITH MANA-DRAINING ATTACKS`,
+        'attackType': 'Melee',
+        'complexity': 1,
+        'spells': ['Mana Break', 'Blink', 'Counterspell', 'Mana Void']
+    },
+    'arc warden': {
+        'type': 'Agility',
+        'mainInfo': `CREATES A COPY OF HIMSELF TO SPLIT PUSH`,
+        'attackType': 'Ranged',
+        'complexity': 3,
+        'spells': ['Flux', 'Magnetic Field', 'Spark Wraith', 'Tempest Double']
+    },
+    'axe': {
+        'type': 'Strength',
+        'mainInfo': `TAUNTS AND FORCES ENEMIES TO ATTACK HIM`,
+        'attackType': 'Melee',
+        'complexity': 1,
+        'spells': ["Berserker's Call", 'Battle Hunger', 'Counter Helix', 'Culling Blade']
+    },
+    'bane': {
+        'type': 'Universal',
+        'mainInfo': `PUTS HIS ENEMIES TO SLEEP, INCAPACITATING THEM`,
+        'attackType': 'Ranged',
+        'complexity': 2,
+        'spells': ['Enfeeble', 'Brain Sap', 'Nightmare', "Fiend's Grip"]
+    },
+    'batrider': {
+        'type': 'Universal',
+        'mainInfo': `CAN LASSO AN ENEMY AWAY FROM HIS TEAM`,
+        'attackType': 'Ranged',
+        'complexity': 2,
+        'spells': ['Sticky Napalm', 'Flamebreak', 'Firefly', 'Flaming Lasso']
+    },
+    'unknown': {
+        'message': 'data not available'
+    }
+}
+
+// 'name': {
+//     'type': ,
+//     'mainInfo': ,
+//     'attackType': ,
+//     'complexity': ,
+//     'spells':
+// }
+
+
+app.get('/', (req,res) => {
+    res.send('<h1>Dota heroes API</h1>')
+})
+
+app.get('/api', (req,res) => {
+    res.json(heroes)
+})
+
+app.get('/api/:dotaHero', (req,res) => {
+    const dotaHero = req.params.dotaHero.toLowerCase()
+    if(heroes[dotaHero]){
+        res.json(heroes[dotaHero])
+    }else{
+        res.json(heroes['unknown'])
+    }
+})
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
