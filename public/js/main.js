39fe3569070bd5aae88dfb0ebcc613e366df0e0a
iatGeo <g.iatridis@protonmail.com>
2024-02-14 20:56:40 +0200

    fixed bug occurying while selecting multiple filters

--- public/js/main.js
+++ public/js/main.js
@@ -27,32 +27,6 @@ async function getHeroes(){
     }
 }
 
-<<<<<<< HEAD
-//Fetching the heroes data to pass on to the CBFs
-async function getHeroes(){
-    try {
-        const response = await fetch(baseUrl)
-        const data = await response.json()
-        const heroes = Object.keys(data).map(key => data[key])
-        return heroes
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-// All eventListeners' CBFs
-async function filterMelee(){
-    try {
-        const data = await getHeroes()
-        data.forEach(hero => {
-            const targetHero = document.querySelector(`.${hero['name']}`)
-
-            if( hero['attackType']!=='Melee' ) targetHero.classList.toggle('hidden')
-
-            checkboxMelee.checked ? checkboxRanged.disabled = true 
-                : checkboxRanged.disabled = false
-        })
-=======
 
 //Fetching the heroes data to pass on to the CBFs
 async function getHeroes(){
@@ -61,7 +35,6 @@ async function getHeroes(){
         const data = await response.json()
         const heroes = Object.keys(data).map(key => data[key])
         return heroes
->>>>>>> a9a56010de700765c431a9d284e55cf72e5859ab
     } catch (error) {
         console.error(error)
     }
@@ -70,23 +43,11 @@ async function getHeroes(){
 // EventListeners' CBFunction
 async function filterHeroes () {
     try {
-<<<<<<< HEAD
-        const data = await getHeroes()
-        data.forEach(hero => {
-            const targetHero = document.querySelector(`.${hero['name']}`)
-
-            if( hero['attackType']!=='Ranged' ) targetHero.classList.toggle('hidden')
-
-            checkboxRanged.checked ? checkboxMelee.disabled = true 
-=======
         //If an 'attack-type' filter is checked, disable the other one
-        checkboxRanged.checked ? checkboxMelee.disabled = true 
->>>>>>> a9a56010de700765c431a9d284e55cf72e5859ab
-                : checkboxMelee.disabled = false
-        checkboxMelee.checked ? checkboxRanged.disabled = true 
-                : checkboxRanged.disabled = false
+        checkboxRanged.checked ? checkboxMelee.disabled = true : checkboxMelee.disabled = false
+        checkboxMelee.checked ? checkboxRanged.disabled = true : checkboxRanged.disabled = false
 
-        //Check which checkboxes are checked
+        //Isolate the filters that are being used
         let filtered = []
         for (let i=0; i<checkboxes.length; i++) {
             if (checkboxes[i].checked) {
